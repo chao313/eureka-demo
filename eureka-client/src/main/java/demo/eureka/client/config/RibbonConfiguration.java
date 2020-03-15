0612281448b2590659544257f3481322445074c3
wanghaichao <m18888106873@163.com>
2020-03-16 00:14:48 +0800

    添加全局的Ribbon均衡策略的配置

--- eureka-client/src/main/java/demo/eureka/client/config/RibbonConfiguration.java
+++ eureka-client/src/main/java/demo/eureka/client/config/RibbonConfiguration.java
@@ -1,5 +1,7 @@
 package demo.eureka.client.config;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,5 +21,15 @@ public class RibbonConfiguration {
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    /**
+     * 只需要添加一个配置类就能更改全局的策略设置
+     *
+     * @return
+     */
+    @Bean
+    public IRule iRule() {
+        return new RandomRule();
     }
 }
